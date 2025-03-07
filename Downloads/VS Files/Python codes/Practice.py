e8fd584cb31c88436662f02f74ec6050642a21a8
Saipriyavadisina <saipriyavadisina@gmail.com>
2025-03-07 15:44:43 +0530

    Arithmatic operations

---
+++ Downloads/VS Files/Python codes/Practice.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+name = "Sai Priya"
+age = 22
+result = 3 + 5
+difference = 5 - 3
+product = 5 * 3
+quotient = 10/2
+print(result)
+print(difference)
+print(product)
+print(quotient)
