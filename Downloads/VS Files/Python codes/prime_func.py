dd033e6df6ca277a57da8345510b8d1a8ef3f370
Saipriyavadisina <saipriyavadisina@gmail.com>
2025-04-20 22:36:22 +0530

    Checking given number is prime or not using function

---
+++ Downloads/VS Files/Python codes/prime_func.py
@@ -0,0 +1,10 @@
+def is_prime(num):
+    if num <= 1:
+        return "Not a prime"
+    for i in range(2, num -1):
+        if num % i == 0:
+            return "Not a prime"
+    return "Prime"
+    
+number = int(input())
+print(is_prime(number))
