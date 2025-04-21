3cb25fe0d48aaf041265d3247726b78ea172d119
Saipriyavadisina <saipriyavadisina@gmail.com>
2025-04-21 20:26:33 +0530

    Prime numbers using function

--- Downloads/VS Files/Python codes/prime_func.py
+++ Downloads/VS Files/Python codes/prime_func.py
@@ -1,7 +1,7 @@
 def is_prime(num):
     if num <= 1:
         return "Not a prime"
-    for i in range(2, num -1):
+    for i in range(2, num):
         if num % i == 0:
             return "Not a prime"
     return "Prime"
